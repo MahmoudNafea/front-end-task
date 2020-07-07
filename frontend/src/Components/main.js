20420be7181b428735ff837b9f56f04eaa7d0057
Mahmoud Nafea <mahmoudnafea19@gmail.com>
2020-07-07 18:43:21 +0800

    game UI

--- frontend/src/Components/main.js
+++ frontend/src/Components/main.js
@@ -6,13 +6,12 @@ const Main = () => {
         <div >
             <Jumbotron>
                 <h1>Welcome to Cards game!</h1>
-                <p> Are you excited?</p>
             </Jumbotron>
             <div className=" container">
                 <div className="col-md-6 offset-md-4">
                     <form >
                         <div className="form-group">
-                            <label > Players</label>
+                            <label> Players</label>
                             <input type="number" className="form-control" placeholder="Number of Players" required />
                         </div>
                         <button type="submit" className="btn btn-outline-primary">Start</button>
