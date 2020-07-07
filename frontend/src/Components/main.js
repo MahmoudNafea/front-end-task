d7d85e99b4e98051a293d8bad7cb0fc737378d46
Mahmoud Nafea <mahmoudnafea19@gmail.com>
2020-07-07 18:41:51 +0800

    starting the server

---
+++ frontend/src/Components/main.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Jumbotron } from 'react-bootstrap';
+
+const Main = () => {
+    return (
+        <div >
+            <Jumbotron>
+                <h1>Welcome to Cards game!</h1>
+                <p> Are you excited?</p>
+            </Jumbotron>
+            <div className=" container">
+                <div className="col-md-6 offset-md-4">
+                    <form >
+                        <div className="form-group">
+                            <label > Players</label>
+                            <input type="number" className="form-control" placeholder="Number of Players" required />
+                        </div>
+                        <button type="submit" className="btn btn-outline-primary">Start</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    )
+};
+
+export default Main;
