9f53d4f2c1b89a01f62ee7cd8ef7449d44aa929b
Mahmoud Nafea <mahmoudnafea19@gmail.com>
2020-07-07 21:42:46 +0800

    creating cards

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,35 @@ const express = require("express");
 const app = express();
 app.use(express.json());
 
+const suits = ["S", "H", "D", "C"];
+const values = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"];
+const cards = [];
+
+for (let i = 0; i < values.length; i++) {
+    for (let x = 0; x < suits.length; x++) {
+        const card = { value: values[i], suit: suits[x] }
+        cards.push(card)
+        // console.log(cards.length)
+    }
+};
+const chunk = 5
+for (let i = 0; i < cards.length; i += chunk) {
+    let result = []
+    result = cards.slice(i, i + chunk)
+    console.log(result)
+}
+
+// const splitCards = (n) => {
+//     const result = []
+//     const forPlayer = Math.ceil(cards / n)
+//     result.push(forPlayer)
+//     // console.log(result)
+// }
+// console.log(splitCards(3))
+
+app.get('/cards', (req, res) => {
+    res.send("cards")
+})
 // app.get("*", (req, res) => {
 //     res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
 // });
